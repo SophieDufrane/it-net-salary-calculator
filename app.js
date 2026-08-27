9da27ea5f6ea7b0959b8b65fdc98ef907b239201
Sophie <dufranesophie@gmail.com>
2026-08-27 12:12:02 +0200

    Display formatted RAL value in results table

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const formEntries = document.getElementById("form-entries");
 const incomeInput = document.getElementById("income-input");
+const resultRal = document.getElementById("result-ral");
 
 formEntries.addEventListener("submit", (event) => {
   event.preventDefault();
   const ral = Number(incomeInput.value);
   const results = calculateNetSalary(ral);
   console.log(results);
+
+  resultRal.textContent = ral.toLocaleString("it-IT");
 });
